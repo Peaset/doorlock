bee7b05124ed0f005fc97ff9210d26475a98600d
Peaset <76628978+Peaset@users.noreply.github.com>
2020-12-28 23:56:45 +0300

    Add files via upload

---
+++ esx_doorlock/config.lua
@@ -0,0 +1,329 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.Draw3DText = "~r~Açık~r~"
+Config.Draw3DTexts = "~y~Açmak~s~ için ~g~[E]~s~  tuşuna bas"
+Config.DoorList = {
+
+	--
+	-- Mission Row First Floor
+	--
+
+	-- Entrance Doors
+	{
+		textCoords = vector3(434.7, -982.0, 31.5),
+		authorizedJobs = { 'police' },
+		locked = false,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'v_ilev_ph_door01',
+				objYaw = -90.0,
+				objCoords = vector3(434.7, -980.6, 30.8)
+			},
+
+			{
+				objName = 'v_ilev_ph_door002',
+				objYaw = -90.0,
+				objCoords = vector3(434.7, -983.2, 30.8)
+			}
+		}
+	},
+
+	-- To locker room & roof
+	{
+		objName = 'v_ilev_ph_gendoor004',
+		objYaw = 90.0,
+		objCoords  = vector3(449.6, -986.4, 30.6),
+		textCoords = vector3(450.1, -986.3, 31.7),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Rooftop
+	{
+		objName = 'v_ilev_gtdoor02',
+		objYaw = 90.0,
+		objCoords  = vector3(464.3, -984.6, 43.8),
+		textCoords = vector3(464.3, -984.0, 44.8),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Hallway to roof
+	{
+		objName = 'v_ilev_arm_secdoor',
+		objYaw = 90.0,
+		objCoords  = vector3(461.2, -985.3, 30.8),
+		textCoords = vector3(461.5, -986.0, 31.5),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Armory
+	{
+		objName = 'v_ilev_arm_secdoor',
+		objYaw = -90.0,
+		objCoords  = vector3(452.6, -982.7, 30.6),
+		textCoords = vector3(453.0, -982.6, 31.7),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Captain Office
+	{
+		objName = 'v_ilev_ph_gendoor002',
+		objYaw = -180.0,
+		objCoords  = vector3(447.2, -980.6, 30.6),
+		textCoords = vector3(447.2, -980.0, 31.7),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- To downstairs (double doors)
+	{
+		textCoords = vector3(444.6, -989.4, 31.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 4,
+		doors = {
+			{
+				objName = 'v_ilev_ph_gendoor005',
+				objYaw = 180.0,
+				objCoords = vector3(443.9, -989.0, 30.6)
+			},
+
+			{
+				objName = 'v_ilev_ph_gendoor005',
+				objYaw = 0.0,
+				objCoords = vector3(445.3, -988.7, 30.6)
+			}
+		}
+	},
+
+	--
+	-- Mission Row Cells
+	--
+
+	-- Main Cells
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objYaw = 0.0,
+		objCoords  = vector3(463.8, -992.6, 24.9),
+		textCoords = vector3(463.3, -992.6, 25.1),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Cell 1
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objYaw = -90.0,
+		objCoords  = vector3(462.3, -993.6, 24.9),
+		textCoords = vector3(461.8, -993.3, 25.0),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Cell 2
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objYaw = 90.0,
+		objCoords  = vector3(462.3, -998.1, 24.9),
+		textCoords = vector3(461.8, -998.8, 25.0),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	-- Cell 3
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objYaw = 90.0,
+		objCoords  = vector3(462.7, -1001.9, 24.9),
+		textCoords = vector3(461.8, -1002.4, 25.0),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objCoords  = vector3(471.21, -1002.19, 24.9),
+		textCoords = vector3(471.75, -1001.92, 25.0),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objCoords  = vector3(471.34, -998.17, 24.9),
+		textCoords = vector3(471.23, -998.3, 25.0),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_ph_cellgate',
+		objCoords  = vector3(471.4, -994.69, 24.9),
+		textCoords = vector3(471.26, -994.27, 25.0),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_cd_entrydoor',
+		objCoords  = vector3(457.59, -982.45, 26.6),
+		textCoords = vector3(457.62, -982.38, 26.6),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_cd_entrydoor',
+		objCoords  = vector3(458.11, -978.82, 26.6),
+		textCoords = vector3(458.43, -978.75, 26.6),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_cd_entrydoor',
+		objCoords  = vector3(457.94, -973.77, 26.6),
+		textCoords = vector3(457.66, -973.69, 26.6),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		objName = 'v_ilev_cd_entrydoor',
+		objCoords  = vector3(457.85, -969.33, 26.6),
+		textCoords = vector3(457.79, -969.28, 26.6),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	{
+		textCoords = vector3(446.47, -986.58, 26.67),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 4,
+		doors = {
+			{
+				objName = 'v_ilev_ph_gendoor006',
+				objCoords  = vector3(446.16, -987.14, 26.5)
+			},
+
+			{
+				objName = 'v_ilev_ph_gendoor006',
+				objCoords  = vector3(446.44, -985.84, 26.5)
+			}
+		}
+	},
+
+	
+	-- To Back
+	{
+		objName = 'v_ilev_gtdoor',
+		objYaw = 0.0,
+		objCoords  = vector3(463.4, -1003.5, 25.0),
+		textCoords = vector3(464.0, -1003.5, 25.5),
+		authorizedJobs = { 'police' },
+		locked = true
+	},
+
+	--
+	-- Mission Row Back
+	--
+
+	-- Back (double doors)
+	{
+		textCoords = vector3(468.6, -1014.4, 27.1),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 4,
+		doors = {
+			{
+				objName = 'v_ilev_rc_door2',
+				objYaw = 0.0,
+				objCoords  = vector3(467.3, -1014.4, 26.5)
+			},
+
+			{
+				objName = 'v_ilev_rc_door2',
+				objYaw = 180.0,
+				objCoords  = vector3(469.9, -1014.4, 26.5)
+			}
+		}
+	},
+
+	-- Back Gate
+	{
+		objName = 'hei_prop_station_gate',
+		objYaw = 90.0,
+		objCoords  = vector3(488.8, -1017.2, 27.1),
+		textCoords = vector3(488.8, -1020.2, 30.0),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 14,
+		size = 2
+	},
+
+	--
+	-- Sandy Shores
+	--
+
+
+	--
+	-- Bolingbroke Penitentiary
+	--
+
+	-- Entrance (Two big gates)
+	{
+		objName = 'prop_gate_prison_01',
+		objCoords  = vector3(1844.9, 2604.8, 44.6),
+		textCoords = vector3(1844.9, 2608.5, 48.0),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 12,
+		size = 2
+	},
+
+	{
+		objName = 'prop_gate_prison_01',
+		objCoords  = vector3(1818.5, 2604.8, 44.6),
+		textCoords = vector3(1818.5, 2608.4, 48.0),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 12,
+		size = 2
+	},
+	
+
+	--[[ principal bank
+	{
+		objName = 'hei_v_ilev_bk_gate2_pris',
+		objCoords  = vector3(261.99899291992, 221.50576782227, 106.68346405029),
+		textCoords = vector3(261.99899291992, 221.50576782227, 107.68346405029),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 12,
+		size = 2
+	}--]]
+
+
+	--
+	-- Addons
+	--
+
+	--[[
+	-- Entrance Gate (Mission Row mod) https://www.gta5-mods.com/maps/mission-row-pd-ymap-fivem-v1
+	{
+		objName = 'prop_gate_airport_01',
+		objCoords  = vector3(420.1, -1017.3, 28.0),
+		textCoords = vector3(420.1, -1021.0, 32.0),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 14,
+		size = 2
+	}
+	--]]
+}
---
+++ esx_doorlock/locales/en.lua
@@ -0,0 +1,5 @@
+Locales ['en'] = {
+  ['unlocked'] = '~g~Açık~s~',
+  ['locked'] = '~r~Kilitli~s~',
+  ['press_button'] = '[E] %s',
+}
